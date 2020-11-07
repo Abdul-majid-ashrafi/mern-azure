263f022491e974f6a9f6c824ed7b00255d1c5d70
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2020-11-07 06:25:37 +0500

    bug solved

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // gives server access to static files, will generate by npm run build 
-app.use(express.static('./client/mern-azure/build/'))
+// app.use(express.static('./client/mern-azure/build/'))
 
 app.get("/", (request, response) => {
-    response.sendFile("index.html", { root: './client/mern-azure/build/' })
+    response.sendFile("index.html", { root: __dirname })
+    // response.sendFile("index.html", { root: '/client/mern-azure/build/' })
 })
 
 
